8811ddce8f222ba9b748c4e25c2f9d5e3978488f
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-06-23 22:06:06 +0200

    init ASP.NET Core Web API project with controllers

---
+++ ClinicAdoNetApi/Controllers/AppointmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicAdoNetApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Appointments endpoint works");
+    }
+}
