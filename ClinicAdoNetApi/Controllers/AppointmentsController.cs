023aeafe708b3cd5597e46ce954d217f0196b2dc
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-06-24 22:07:22 +0200

    implement get appointments endpoint

--- ClinicAdoNetApi/Controllers/AppointmentsController.cs
+++ ClinicAdoNetApi/Controllers/AppointmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using ClinicAdoNetApi.DTOs;
 
 namespace ClinicAdoNetApi.Controllers;
 
@@ -17,21 +18,47 @@ public class AppointmentsController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get()
     {
+        var appointments = new List<AppointmentListDto>();
+
         var connectionString =
             _configuration.GetConnectionString("DefaultConnection");
 
-        try
-        {
-            await using var connection =
-                new SqlConnection(connectionString);
+        await using var connection =
+            new SqlConnection(connectionString);
 
-            await connection.OpenAsync();
+        await connection.OpenAsync();
 
-            return Ok("Database connection successful");
-        }
-        catch (Exception ex)
+        const string sql = @"
+        SELECT
+            a.IdAppointment,
+            a.AppointmentDate,
+            a.Status,
+            a.Reason,
+            p.FirstName + N' ' + p.LastName AS PatientFullName,
+            p.Email AS PatientEmail
+        FROM dbo.Appointments a
+        JOIN dbo.Patients p ON p.IdPatient = a.IdPatient
+        ORDER BY a.AppointmentDate";
+
+        await using var command =
+            new SqlCommand(sql, connection);
+
+        await using var reader =
+            await command.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
         {
-            return BadRequest(ex.Message);
+            appointments.Add(new AppointmentListDto
+            {
+                IdAppointment = reader.GetInt32(0),
+                AppointmentDate = reader.GetDateTime(1),
+                Status = reader.GetString(2),
+                Reason = reader.GetString(3),
+                PatientFullName = reader.GetString(4),
+                PatientEmail = reader.GetString(5)
+            });
         }
+
+        return Ok(appointments);
     }
 }
