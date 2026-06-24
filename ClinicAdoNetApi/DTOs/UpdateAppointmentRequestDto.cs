8cd428327a3776d386e23fac2e1d02753648fa22
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-06-24 22:04:14 +0200

    add appointment dto models

---
+++ ClinicAdoNetApi/DTOs/UpdateAppointmentRequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ClinicAdoNetApi.DTOs;
+
+public class UpdateAppointmentRequestDto
+{
+    public int IdPatient { get; set; }
+    public int IdDoctor { get; set; }
+    public DateTime AppointmentDate { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+    public string Reason { get; set; } = string.Empty;
+
+    public string? InternalNotes { get; set; }
+}
